faa96f4d619d6b4410c6592bb89428879a91cefc
hikem1 <hikem1@hotmail.fr>
2026-01-22 20:00:24 +0100

    update script

--- myscript.user.js
+++ myscript.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/hikem1/tm-example
 // @version      1.0.0
 // @description  Exemple Tampermonkey développé avec VS Code et GitHub
-// @match        https://example.com/*
+// @match *://*/*
 // @updateURL    https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js
 // @downloadURL  https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js
 // @grant        none
