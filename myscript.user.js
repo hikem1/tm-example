53e408d83713281ef0f4a7b556317b0e428566c4
hikem1 <hikem1@hotmail.fr>
2026-01-23 13:35:31 +0100

    up 2

--- myscript.user.js
+++ myscript.user.js
@@ -4,7 +4,7 @@
 // @version      1.0.0.8
 // @description  Exemple Tampermonkey développé avec VS Code et GitHub
 // @match           *://*/*
-// @updateURL    https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js?v=1.0.0.8
+// @updateURL    https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js
 // @downloadURL  https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js
 // @grant        none
 // @require      https://raw.githubusercontent.com/hikem1/tm-example/main/lib.js
