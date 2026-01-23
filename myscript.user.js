192e036081af5b5a2db27f0e916e8cc020147b94
hikem1 <hikem1@hotmail.fr>
2026-01-23 07:23:17 +0100

    update script3

--- myscript.user.js
+++ myscript.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         TM Example – Hello World
 // @namespace    https://github.com/hikem1/tm-example
-// @version      1.0.6
+// @version      1
 // @description  Exemple Tampermonkey développé avec VS Code et GitHub
 // @match           *://*/*
-// @updateURL    https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js
-// @downloadURL  https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js
+// @updateURL    https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js?v=1
+// @downloadURL  https://raw.githubusercontent.com/hikem1/tm-example/main/myscript.user.js?v=1
 // @grant        none
-// @require      https://raw.githubusercontent.com/hikem1/tm-example/main/lib.js
+// @require      https://raw.githubusercontent.com/hikem1/tm-example/main/lib.js?v=1
 // ==/UserScript==
 
 (function () {
